840c2e594cba26c1c0973c640a5025bcbbf88910
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-09-06 20:57:30 +0530

    [Abhi]Added:Line Computation program added

--- Line_Comparison/Program.cs
+++ Line_Comparison/Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to line comparison program");
+            Console.WriteLine("Choose the option \n 1.Line Computation program added");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1: Class1.LineComputation();break;
+            }
         }
     }
 }
