f1de0fd00f59d5e844d57aaf5e94c6c22b7e4b15
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-09-06 21:10:15 +0530

    [Abhi]Added:Line comparison by two points

--- Line_Comparison/Program.cs
+++ Line_Comparison/Program.cs
@@ -5,13 +5,14 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to line comparison program");
-            Console.WriteLine("Choose the option \n 1.Line Computation program added \n 2.Line comparison between two line");
+            Console.WriteLine("Choose the option \n 1.Line Computation program added \n 2.Line comparison between two line \n 3.line compared by points");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
             {
                 case 1: Class1.LineComputation();break;
                 case 2: Class2.LineComparison();break;
+                case 3: Class3.LineEquality();break;
             }
         }
     }
