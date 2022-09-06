5b0b79c6914ea1c84b283d1c44e3a3e5ad63d026
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-09-06 21:14:48 +0530

    [Abhi]Added:Using comparison to method

--- Line_Comparison/Program.cs
+++ Line_Comparison/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to line comparison program");
-            Console.WriteLine("Choose the option \n 1.Line Computation program added \n 2.Line comparison between two line \n 3.line compared by points");
+            Console.WriteLine("Choose the option \n 1.Line Computation program added \n 2.Line comparison between two line \n 3.line compared by points \n 4.line compared by comparison method");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -13,6 +13,7 @@
                 case 1: Class1.LineComputation();break;
                 case 2: Class2.LineComparison();break;
                 case 3: Class3.LineEquality();break;
+                case 4: Class4.ComparisonMethod();break;
             }
         }
     }
